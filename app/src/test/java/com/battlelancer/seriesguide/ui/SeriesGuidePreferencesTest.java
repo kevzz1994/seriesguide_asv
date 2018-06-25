04152fad99937393a29ea8708989f26551273f0f
Sergio Bijlhout <>
2018-06-25 22:29:33 +0200

    x

--- app/src/test/java/com/battlelancer/seriesguide/ui/SeriesGuidePreferencesTest.java
+++ app/src/test/java/com/battlelancer/seriesguide/ui/SeriesGuidePreferencesTest.java
@@ -1,17 +1,25 @@
 package com.battlelancer.seriesguide.ui;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * StatsFragmentTest
@@ -20,38 +28,48 @@ import org.mockito.junit.MockitoRule;
 @RunWith(MockitoJUnitRunner.class)
 public class SeriesGuidePreferencesTest {
 
+
     public static final String KEY_LANGUAGE_PREFERRED = "language";
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Mock
-    SeriesGuidePreferences seriesGuidePreferences;
+    Context context;
 
     @Mock
-    Bundle savedInstanceState;
+    SharedPreferences.Editor editor;
 
     @Mock
-    SharedPreferences preferenceManager;
+    PreferenceManager preferenceManager;
 
+    @Mock
+    SharedPreferences sharedPreferences;
+
+    @Before
+    public void setup() {
+        context = mock(Context.class);
+        preferenceManager = mock(PreferenceManager.class);
+        editor = mock(SharedPreferences.Editor.class);
+        sharedPreferences = mock(SharedPreferences.class);
+        when(preferenceManager.getSharedPreferences().getString(anyString(), anyString())).thenReturn(anyString());
+        when(preferenceManager.getSharedPreferences().edit()).thenReturn(editor);
+        when(preferenceManager.getSharedPreferences().edit().putString(anyString(), anyString())).thenReturn(editor);
+    }
     @Test
     public void languageChangeTest() {
 
-        ActionBar actionBar;
-
-        //Given
+        final String DEFAULT_LANGUAGE = "en";
         final String LANGUAGE_FR = "fr";
-        preferenceManager.edit().putString(KEY_LANGUAGE_PREFERRED, LANGUAGE_FR).commit();
+        final String LANGUAGE_DE = "de";
 
-        //When
-        seriesGuidePreferences.onCreate(savedInstanceState);
+        preferenceManager.getSharedPreferences().edit().putString(KEY_LANGUAGE_PREFERRED, LANGUAGE_FR).apply();
+        String frenchLanguage = preferenceManager.getSharedPreferences().getString(KEY_LANGUAGE_PREFERRED, DEFAULT_LANGUAGE);
 
-        //Then
-        actionBar = seriesGuidePreferences.getActionBar();
-        String test = (String) actionBar.getTitle();
-        System.out.println(test);
+        preferenceManager.getSharedPreferences().edit().putString(KEY_LANGUAGE_PREFERRED, LANGUAGE_DE).apply();
+        String deutchLanguage = preferenceManager.getSharedPreferences().getString(KEY_LANGUAGE_PREFERRED, DEFAULT_LANGUAGE);
 
-
+        System.out.println(frenchLanguage + deutchLanguage);
     }
 
 }
